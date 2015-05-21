07d00ddb56541cb6f1e283bd5f17221889f53a80
Serkan Sendur <serkan_sendur@hotmail.com>
2015-05-21 16:18:51 +0300

    First time commit

---
+++ SerqTime.EasyTimeZone/frmTimeByZone.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace SerqTime.EasyTimeZone
+{
+    partial class frmTimeByZone
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.cbTimeZone = new System.Windows.Forms.ComboBox();
+            this.tmrTime = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Time:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Timezone:";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(98, 64);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(207, 20);
+            this.txtTime.TabIndex = 5;
+            // 
+            // cbTimeZone
+            // 
+            this.cbTimeZone.FormattingEnabled = true;
+            this.cbTimeZone.Location = new System.Drawing.Point(98, 19);
+            this.cbTimeZone.Name = "cbTimeZone";
+            this.cbTimeZone.Size = new System.Drawing.Size(207, 21);
+            this.cbTimeZone.TabIndex = 4;
+            // 
+            // tmrTime
+            // 
+            this.tmrTime.Tick += new System.EventHandler(this.tmrTime_Tick);
+            // 
+            // frmTimeByZone
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 142);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.cbTimeZone);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(396, 180);
+            this.MinimumSize = new System.Drawing.Size(396, 180);
+            this.Name = "frmTimeByZone";
+            this.Text = "Time by zone";
+            this.Load += new System.EventHandler(this.frmTimeByZone_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.ComboBox cbTimeZone;
+        private System.Windows.Forms.Timer tmrTime;
+    }
+}
+
---
+++ SerqTime.EasyTimeZone/frmTimeByZone.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerqTime.EasyTimeZone
+{
+    public partial class frmTimeByZone : Form
+    {
+        public frmTimeByZone()
+        {
+            InitializeComponent();
+        }
+
+        private void tmrTime_Tick(object sender, EventArgs e)
+        {
+            TimeZoneInfo destinationTimeZone = TimeZoneInfo.FindSystemTimeZoneById(cbTimeZone.SelectedItem.ToString());
+            DateTime destinationTime = TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.Local, destinationTimeZone);
+            txtTime.Text = destinationTime.ToString();
+        }
+
+        private void frmTimeByZone_Load(object sender, EventArgs e)
+        {
+            foreach (TimeZoneInfo z in TimeZoneInfo.GetSystemTimeZones())
+            {
+                cbTimeZone.Items.Add(z.Id);
+            }
+            cbTimeZone.SelectedItem = TimeZoneInfo.Local.StandardName; //  TimeZone.CurrentTimeZone.StandardName also works.
+            tmrTime.Start();
+        }
+    }
+}
